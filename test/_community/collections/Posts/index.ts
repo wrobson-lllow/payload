1337c35ade2015609e6003bcb2f5b5200ce99724
William Robson <williamrobson@lllow.com>
2024-07-24 07:52:06 -0400

    add CustomAPIError and beforeDelete hook to Posts

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -1,11 +1,25 @@
 import type { CollectionConfig } from '../../../../packages/payload/src/collections/config/types'
+import { APIError } from '../../../../packages/payload/src/errors'
 
 import { mediaSlug } from '../Media'
 
 export const postsSlug = 'posts'
 
+class CustomAPIError extends APIError {
+  constructor(message: string) {
+    super(message, 400, undefined, true)
+  }
+}
+
 export const PostsCollection: CollectionConfig = {
   defaultSort: 'title',
+  hooks: {
+    beforeDelete: [
+      () => {
+        throw new CustomAPIError('You must select the delete checkbox before deleting this post')
+      },
+    ],
+  },
   fields: [
     {
       name: 'text',
